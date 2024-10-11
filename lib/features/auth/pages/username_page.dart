02ce4e6da7b912bafe2be58ef30f639db1c1ace2
Mohammed Samy <mom118580@gmail.com>
2024-10-11 06:05:17 -0700

    Refactore Username Page and Validation logic username

--- lib/features/auth/pages/username_page.dart
+++ lib/features/auth/pages/username_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:you_tube/core/widgets/flat_button.dart';
@@ -13,6 +15,26 @@ class UsernamePage extends StatefulWidget {
 
 class _UsernamePageState extends State<UsernamePage> {
   final TextEditingController usernameController = TextEditingController();
+  bool isValidate = true;
+
+  void validateUsername() async {
+    final userMap = await FirebaseFirestore.instance.collection("Users").get();
+
+    final users = userMap.docs.map((user) => user).toList();
+    String? targeredUsername;
+
+    for (var user in users) {
+      if (usernameController.text == user.data()["username"]) {
+        targeredUsername = user.data()["username"];
+        isValidate = false;
+        setState(() {});
+      }
+      if (usernameController.text != targeredUsername) {
+        isValidate = true;
+        setState(() {});
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,11 +56,18 @@ class _UsernamePageState extends State<UsernamePage> {
               padding: const EdgeInsets.only(left: 15, right: 15),
               child: Form(
                 child: TextFormField(
+                  onChanged: (username) {
+                    validateUsername();
+                  },
+                  autovalidateMode: AutovalidateMode.always,
+                  validator: (username) {},
                   key: formKey,
                   controller: usernameController,
                   decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.verified_user_rounded),
-                    suffixIconColor: Colors.green,
+                    suffixIcon: isValidate
+                        ? Icon(Icons.verified_user_rounded)
+                        : Icon(Icons.cancel),
+                    suffixIconColor: isValidate ? Colors.green : Colors.red,
                     hintText: "Insery Username",
                     border: OutlineInputBorder(
                       borderSide: BorderSide(color: Colors.blue),
@@ -58,8 +87,10 @@ class _UsernamePageState extends State<UsernamePage> {
               padding: const EdgeInsets.only(bottom: 30, left: 8, right: 8),
               child: FlatButton(
                 text: "CONTINUE",
-                onPressed: () {},
-                colour: Colors.green,
+                onPressed: () {
+                  // add user data inside database
+                },
+                colour: isValidate ? Colors.green : Colors.green.shade100,
               ),
             ),
           ],
